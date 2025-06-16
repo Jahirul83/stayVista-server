545ddf2b891e5abc4ed0d9fa4e80f995ca96f89f
Jahirul83 <jahirulislamcse83@gmail.com>
2025-06-16 12:22:10 +0600

    get method for rooms

--- index.js
+++ index.js
@@ -48,6 +48,8 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    
+    const roomCollection = client.db('stayvista').collection('rooms')
     // auth related api
     app.post('/jwt', async (req, res) => {
       const user = req.body
@@ -76,6 +78,12 @@ async function run() {
       } catch (err) {
         res.status(500).send(err)
       }
+    })
+
+    // get all rooms from db
+    app.get('/rooms', async (req, res) => {
+      const result  = await roomCollection.find().toArray();
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
